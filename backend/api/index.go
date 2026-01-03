e1f13a5324c01c42f4b6de50b4993a0fe76c2cfd
Rozag <www.rozag@gmail.com>
2026-01-03 18:46:35 +0700

    delete /v1 and v2, edit / routego api

--- backend/api/index.go
+++ backend/api/index.go
@@ -19,22 +19,22 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func handler() http.HandlerFunc {
  app := fiber.New()
 
- app.Get("/v1", func(ctx *fiber.Ctx) error {
-  return ctx.JSON(fiber.Map{
-   "version": "v1",
-  })
- })
+//  app.Get("/v1", func(ctx *fiber.Ctx) error {
+//   return ctx.JSON(fiber.Map{
+//    "version": "v1",
+//   })
+//  })
 
- app.Get("/v2", func(ctx *fiber.Ctx) error {
-  return ctx.JSON(fiber.Map{
-   "version": "v2",
-  })
- })
+//  app.Get("/v2", func(ctx *fiber.Ctx) error {
+//   return ctx.JSON(fiber.Map{
+//    "version": "v2",
+//   })
+//  })
 
  app.Get("/", func(ctx *fiber.Ctx) error {
   return ctx.JSON(fiber.Map{
-   "uri":  ctx.Request().URI().String(),
-   "path": ctx.Path(),
+   "msg1":  "Hello World",
+   "msg2":  "Welcome to Kahoot Clone Backend",
   })
  })
 
